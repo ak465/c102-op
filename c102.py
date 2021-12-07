c75a0d116a9f9535e48fc888758f8246ba6ad787
ak465 <76836592+ak465@users.noreply.github.com>
2021-12-07 20:39:24 +0530

    Update c102.py

--- c102.py
+++ c102.py
@@ -1,53 +1,30 @@
-import cv2
-import dropbox
-import time
-import random
+while True:
+    print("1 Addition")
+    print("2 Subtraction")
+    print("3 Multiplication")
+    print("4 Division")
+    print("Enter q or Q to Exit")
 
-startTime=time.time()
+    choice = input("Enter your choice: ")
+    if choice =="q" or choice =="Q":
+        break
 
-def takesnap():
-    num = random.randint(0, 100)
-    VideoCaptureObject=cv2.VideoCapture(0)
-    result=True
-    while(result):
-        ret,frame=VideoCaptureObject.read()
-        imageName="img"+str(num)+".png"
-        cv2.imwrite(imageName,frame)
-        result=False
+    num1 = float(input("Enter Number 1: "))
+    num2 = float(input("Enter Number 2: "))
 
-    VideoCaptureObject.release()
-    cv2.destroyAllWindows()
-    print("you are looking good bro but snapshoot taken bro") 
-    return imageName  
-    
+    if choice == "1":
+        print(num1, "+", num2, "=", (num1+num2))
+    elif choice == "2":
+        print(num1, "-", num2, "=", (num1-num2))
+    elif choice == "3":
+        print(num1, "*", num2, "=", (num1*num2))
+    elif choice == "4":
+     if num2 == 0.0:
+        print("Divide by 0 error")
+    else:
+        print(num1, "/", num2, "=", (num1/num2)) 
 
-def uploadFiles(imageName):
-    accessToken="loXN1nTcPW8AAAAAAAAAAaXXT63mbhD3rFzeG8BQK6722K4Wg8gHtSN8K6t1Tf0_"
-    file_from=imageName
-    file_to="/own images/"+ imageName
-    dbx = dropbox.Dropbox(accessToken)
-    with open(file_from, 'rb') as f:
-        dbx.files_upload(f.read(), file_to,mode=dropbox.files.WriteMode.overwrite)
-        print("file uploaded bro")
-        
-def main():
-    while(True):
-        if(time.time()-startTime>=1):
-            name=takesnap()
-            uploadFiles(name)
-
-
-main()
-
-
-
-
-
-
-# imwrite accepts 2 things
-#     -name of the image which you are saving
-#     -which image you are saving
-
-
+else:
+   print("Invalid Choice")
 
 
